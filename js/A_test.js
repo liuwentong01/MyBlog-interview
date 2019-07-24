f8fa3338bff03e0e9c59bd7fbfd6ac7b157cda98
qzuser <1522502267@qq.com>
2019-07-24 15:36:57 +0800

    ++

--- js/A_test.js
+++ js/A_test.js
@@ -1,18 +1,7 @@
-let nAdd;
-let t = () => {
-  let n = 99;
-  nAdd = () => {
-    n++;
-  };
-  let t2 = () => {
-    console.log(n);
-  };
-  return t2;
-};
-
-let a1 = t();
-let a2 = t();
-
-nAdd();
-a1(); //99
-a2(); //100
+for (var i = 0; i < 5; i++) {
+    (function(j){
+      setTimeout(function() {
+        console.log(new Date, j);
+    }, 1000);
+})(i)
+}
