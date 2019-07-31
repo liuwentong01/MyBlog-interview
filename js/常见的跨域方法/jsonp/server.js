d6a342fdef70bec5cb22ce44951869f3a26f9798
qzuser <1522502267@qq.com>
2019-07-31 19:30:24 +0800

    .

--- js/常见的跨域方法/jsonp/server.js
+++ js/常见的跨域方法/jsonp/server.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let app = express();
 app.get('/say', function(req, res) {
+   res.setHeader("Content-type", "text/plain");
   let { wd, callback } = req.query
   res.end(`${callback}('我不爱你')`)
 })
