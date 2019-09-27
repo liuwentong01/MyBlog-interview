8bd257467c3e855f866dcff5f95b0bc669497d4b
qzuser <1522502267@qq.com>
2019-09-27 09:31:04 +0800

    。

--- A_test.js
+++ A_test.js
@@ -1,13 +1,10 @@
-function insertionSort(array) {
-  for (var i = 1; i < array.length; i++) {
-    for(j = i - 1; j >= 0 && array[j] > array[j+1]; j--) {
-      var temp = array[j];
-      array[j] = array[j+1];
-      array[j+1] = temp;
-    }
+//快速幂
+function as(x,n){
+  var res = 1;
+  while(n){
+    if(n & 1) res *= x;
+    x *= x;
+    n >>= 1; 
   }
-  return array;
+  console.log(res);
 }
-
-var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
-console.log(insertionSort(arr));
