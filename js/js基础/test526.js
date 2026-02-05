d28feadc0324fd021ac6c5897386a0ebde6d05e5
liuwentong <liuwentong@huohua.cn>
2026-02-05 23:38:19 +0800

    MOD(master): debug

--- js/js基础/test526.js
+++ js/js基础/test526.js
@@ -1,33 +1,33 @@
-let a = 'a5'.split('');
+let a = "a5".split("");
 let max = 0;
 let tmp = 0;
 let count = 0;
-for(let i = 0; i < a.length; i++) {
-    if(isNaN(parseInt(a[i]))) {
-        count++;
+for (let i = 0; i < a.length; i++) {
+  if (isNaN(parseInt(a[i]))) {
+    count++;
+  }
+}
+if (count === a.length) {
+  return -1;
+}
+for (let i = 0; i < a.length; i++) {
+  if (isNaN(parseInt(a[i]))) {
+    a[i] = "0";
+    // console.log(i, a[i], '为撒谎')
+  } else {
+    continue;
+  }
+  console.log(a);
+  for (let j = 0; j < a.length; j++) {
+    if (!isNaN(parseInt(a[j]))) {
+      console.log(a[j], "@1");
+      tmp++;
+    } else {
+      tmp = 0;
     }
-}
-if(count === a.length) {
-    return -1
-}
-for(let i = 0; i < a.length; i++) {
-	if(isNaN(parseInt(a[i]))) {
-		a[i] = '0';
-		// console.log(i, a[i], '为撒谎')
-	} else {
-		continue;
-	}
-	console.log(a);
-	for(let j = 0; j < a.length; j++) {
-		if(!isNaN(parseInt(a[j]))) {
-			console.log(a[j], '@1')
-			tmp++;
-		} else {
-			tmp = 0;
-		}
-		max = Math.max(tmp, max);
-	}
-	a[i] = 'a';
+    max = Math.max(tmp, max);
+  }
+  a[i] = "a";
 }
 console.log(max);
-
+console.log(111);
