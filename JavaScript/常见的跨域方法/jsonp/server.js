e2dce006c8f33e839f2e358df2a257c3d0a00d14
Riven <liuwentong03@gmail.com>
2026-03-11 23:11:36 +0800

    MOD(master): 目录结构修改

--- JavaScript/常见的跨域方法/jsonp/server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express')
-let app = express();
-app.get('/say', function(req, res) {
-   res.setHeader("Content-type", "text/plain");
-  let { wd, callback } = req.query
-  res.end(`${callback}('我不爱你')`)
-})
-app.listen(3000)
-
